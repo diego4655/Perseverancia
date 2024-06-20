c67123287e149d1e818deaf33af1e636a2c571d8
Diego Perez <43558770+diego4655@users.noreply.github.com>
2024-06-20 12:15:21 -0500

    inicio de creacion de componentes atomicos usando ng bootstrap y standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { NavRegularComponent } from './nav-regular/nav-regular.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [HeaderComponent, NavRegularComponent],
 })
 export class AppComponent {
   title = 'Perseverancia';
